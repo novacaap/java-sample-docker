c8e5b2059ddbed4b190d8de4ae4ca7bbc6079e85
Kalpesh patel <kalpesh@novacaap.com>
2026-03-16 16:06:11 +0530

    update

--- src/main/java/com/dockerforjavadevelopers/hello/config/OpenApiConfig.java
+++ src/main/java/com/dockerforjavadevelopers/hello/config/OpenApiConfig.java
@@ -3,20 +3,36 @@ package com.dockerforjavadevelopers.hello.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${api.external.scheme:https}")
+    private String externalScheme;
+
+    @Value("${api.external.host:localhost:8090}")
+    private String externalHost;
+
+    @Value("${api.external.base-path:/}")
+    private String externalBasePath;
+
     @Bean
     public OpenAPI customOpenAPI() {
+        String url = externalScheme + "://" + externalHost + externalBasePath;
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Java 21 Spring Boot Sample API")
                         .version("1.0.0")
                         .description("Sample REST API with Swagger UI - Java 21 & Spring Boot 3")
                         .contact(new Contact()
-                                .name("Sample App")));
+                                .name("Sample App")))
+                .servers(List.of(new Server().url(url)));
     }
 }
